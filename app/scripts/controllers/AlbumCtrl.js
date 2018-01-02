df56e06cd4ca7b20ef0bb57622314b7f43f1875e
David Alderson <dsa027@gmail.com>
2018-01-02 15:47:40 -0700

    Use Fixtures in Collection

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,13 +1,13 @@
 (function() {
      function AlbumCtrl(Fixtures) {
        this.albumData = Fixtures.getAlbum()
-     }
 
-     this.formatTime = function(time) {
-       return Math.floor(thisAlbum.duration/60) + ":" + Math.floor(thisAlbum.duration%60)
+       AlbumCtrl.formatTime = function(time) {
+         return Math.floor(thisAlbum.duration/60) + ":" + Math.floor(thisAlbum.duration%60)
+       }
      }
 
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl)
+         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl])
  })()
