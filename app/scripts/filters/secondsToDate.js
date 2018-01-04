b1987352dc47e6de51bafb27bfc9dfee74489d6e
David Alderson <dsa027@gmail.com>
2018-01-03 19:37:09 -0700

    Add NaN support in time filter

--- app/scripts/filters/secondsToDate.js
+++ app/scripts/filters/secondsToDate.js
@@ -1,7 +1,11 @@
-angular
+(function() {
+  angular
    .module('blocJams')
    .filter('secondsToDate', [function() {
      return function(seconds) {
+       if (Number.isNaN(seconds)) return 0
+
        return new Date(1970, 0, 1).setSeconds(seconds)
      }
    }])
+})()
